38d2177ddf8f4189b6f700f33632bb5ac63f7260
smittal008 <shower008>
2019-08-26 21:46:24 +0530

    db name change

--- php_laravel_cars_db.sql
+++ php_laravel_cars_db.sql
@@ -2,7 +2,7 @@
 -- Host:                         127.0.0.1
 -- Server version:               10.1.38-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
--- HeidiSQL Version:             10.1.0.5464
+-- HeidiSQL Version:             10.2.0.5599
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -11,7 +11,31 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping structure for table laravelapp3_db.cars
+
+-- Dumping database structure for php_laravel_cars_db
+CREATE DATABASE IF NOT EXISTS `php_laravel_cars_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `php_laravel_cars_db`;
+
+-- Dumping structure for table php_laravel_cars_db.bikes
+CREATE TABLE IF NOT EXISTS `bikes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `make` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `model` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `produced_on` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `color` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `size` varchar(120) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=COMPACT;
+
+-- Dumping data for table php_laravel_cars_db.bikes: ~0 rows (approximately)
+/*!40000 ALTER TABLE `bikes` DISABLE KEYS */;
+INSERT INTO `bikes` (`id`, `make`, `model`, `produced_on`, `created_at`, `updated_at`, `color`, `size`) VALUES
+	(4, 'Bajaj', 'Pulsar', '2015-08-24', NULL, NULL, 'black', NULL);
+/*!40000 ALTER TABLE `bikes` ENABLE KEYS */;
+
+-- Dumping structure for table php_laravel_cars_db.cars
 CREATE TABLE IF NOT EXISTS `cars` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `make` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -19,17 +43,19 @@ CREATE TABLE IF NOT EXISTS `cars` (
   `produced_on` date NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
+  `color` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table laravelapp3_db.cars: ~2 rows (approximately)
+-- Dumping data for table php_laravel_cars_db.cars: ~3 rows (approximately)
 /*!40000 ALTER TABLE `cars` DISABLE KEYS */;
-INSERT INTO `cars` (`id`, `make`, `model`, `produced_on`, `created_at`, `updated_at`) VALUES
-	(1, 'Toyota', 'Corolla', '2019-03-27', '2019-03-27 14:32:55', '2019-03-27 13:11:18'),
-	(2, 'Chevy', 'Silverado', '2009-02-21', '2019-03-27 12:17:20', '2019-03-27 12:17:20');
+INSERT INTO `cars` (`id`, `make`, `model`, `produced_on`, `created_at`, `updated_at`, `color`) VALUES
+	(1, 'Toyota', 'Corolla', '2019-03-27', '2019-03-27 14:32:55', '2019-03-27 13:11:18', 'silver'),
+	(2, 'Chevy', 'Silverado', '2009-02-21', '2019-03-27 12:17:20', '2019-03-27 12:17:20', 'red'),
+	(3, 'Ford', 'Escort', '1997-08-01', '2019-08-23 19:53:28', '2019-08-23 19:53:28', 'cream');
 /*!40000 ALTER TABLE `cars` ENABLE KEYS */;
 
--- Dumping structure for table laravelapp3_db.migrations
+-- Dumping structure for table php_laravel_cars_db.migrations
 CREATE TABLE IF NOT EXISTS `migrations` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -37,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table laravelapp3_db.migrations: ~3 rows (approximately)
+-- Dumping data for table php_laravel_cars_db.migrations: ~3 rows (approximately)
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(1, '2014_10_12_000000_create_users_table', 1),
@@ -45,7 +71,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(3, '2019_03_26_114833_create_cars_table', 1);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 
--- Dumping structure for table laravelapp3_db.password_resets
+-- Dumping structure for table php_laravel_cars_db.password_resets
 CREATE TABLE IF NOT EXISTS `password_resets` (
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -53,11 +79,11 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
   KEY `password_resets_email_index` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table laravelapp3_db.password_resets: ~0 rows (approximately)
+-- Dumping data for table php_laravel_cars_db.password_resets: ~0 rows (approximately)
 /*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
 /*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
 
--- Dumping structure for table laravelapp3_db.users
+-- Dumping structure for table php_laravel_cars_db.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -70,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `users_email_unique` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table laravelapp3_db.users: ~0 rows (approximately)
+-- Dumping data for table php_laravel_cars_db.users: ~0 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
